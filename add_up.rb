2a3a19398bb50f6d55a4e99d3bcb9c585319b76a
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-10 18:25:54 -0500

    completed add up

--- add_up.rb
+++ add_up.rb
@@ -2,3 +2,15 @@
 #and it will add all the numbers from 1 to that integer and return the sum. 
 #Call the function three times within the program, 
 #and each time print out the return value. Call the program add_up.rb.
+
+def add_up(i)
+#formula used 1 + 2 + ... + n = N(n+1)/2
+return i * (i+1) / 2;
+    
+end
+
+p add_up(8)
+
+p add_up(10)
+
+add_up(6)
