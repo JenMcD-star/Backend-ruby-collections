963bd9a15afa8fc2ef04db62a1923e971def56a8
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-10 19:11:04 -0500

    completed leap year

--- leap_year.rb
+++ leap_year.rb
@@ -1,10 +1,17 @@
+def leap_year
+    puts "Pick a start year"
+    start_year = gets.chomp.to_i
 
+    puts "Pick an end year"
+    end_year = gets.chomp.to_i
 
-(4) Write a program, leap_year.rb. 
-It will ask the user for a starting year and an ending year, and it will 
-then print out all the leap years between them, including the starting or ending year 
-if those are leap years. The rules for leap years are: A leap year is divisible by 4,
- except for years that are divisible by 100 -- 
-those aren't leap years -- except for years that are divisible by 400, which ARE leap years.
+    while start_year <= end_year        
+        if start_year % 4 == 0 && start_year % 100 != 0 || start_year % 400 == 0
+            p start_year
+        end
 
+        start_year = start_year + 1
+    end
+end 
 
+leap_year
