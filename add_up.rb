b7281dc2c5696402a896d4ce7f0bdab42d82de77
JenMcD-Star <jenniferannvancura@gmail.com>
2023-10-16 08:19:19 -0500

    reformatted with a loop

--- add_up.rb
+++ add_up.rb
@@ -4,8 +4,23 @@ return i * (i+1) / 2;
     
 end
 
-p add_up(8)
 
-p add_up(10)
 
-add_up(6)
+def add_up_loop(i)
+    if i < 0
+        return
+    end
+  
+    sum = 0
+  
+    (1..i).each do |j|
+        sum += j
+    end
+    return sum 
+  end
+        
+  p add_up_loop(8)
+  
+  p add_up_loop(10)
+  
+ p add_up_loop(6)
